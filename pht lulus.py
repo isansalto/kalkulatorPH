ecc0ce5a835087bca0ac566aff50a7a25afbb8a0
TinksSkuyyy <thegabut25@gmail.com>
2025-04-26 08:52:20 +0700

    Add files via upload

---
+++ pht lulus.py
@@ -0,0 +1,14 @@
+#PENENTUAN KELULUSAN
+print('Penentuan Kelulusan')
+print('...................')
+
+nilai=int(input('Nilai Anda= '))
+KKM=int('60')
+
+if nilai>=KKM:
+    Lulus=True
+else:
+    Lulus=False
+
+#Tampilkan Hasil
+print('Lulus= ',Lulus)
